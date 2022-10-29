bd4d5e67f21b89a1dbcf41fc2737bd97d7f03e61
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-29 19:27:34 +0000

    imprime "JAVA"

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("");
+    System.out.println("   J      a        V       V      a ");
+    System.out.println("   J    a   a       V     V     a   a");
+    System.out.println("J  J   aaaaaaa       V   V     aaaaaaa");
+    System.out.println(" JJ   a       a        V      a       a ");
+    
+  }
+}
